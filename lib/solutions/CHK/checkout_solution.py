5068faaf3bd79e0f512a90b72072873b5c34545b
sbx_user1051 <sbx_user1051@169.254.27.157>
2023-07-23 09:03:19 +0000

    Sun Jul 23 09:03:19 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -10,14 +10,15 @@ class SpecialOffer:
 
 @dataclass
 class SKU:
-    """Represents one type of product"""
+    """
+    Represents one type of product
+    For now it has a name and a price, plus optionally a special offer.
+    We will assume that it could have multiple types of offer later,
+    but simplify a little and do it for only one now until we know more.
+    """
     name: str
     price: int
-    offers: list[SpecialOffer()]
-    special_offer_count: int | None = field(init=False)
-
-    def __post_init__(self):
-        self.c = self.a + self.b
+    offer: SpecialOffer | None
 
 
 
@@ -36,5 +37,6 @@ def checkout(skus: str):
     ``AABBCCC`` to mean, eg, [2xA, 2xB, 3xC] for now,
     and correct it later on if needed depending on how the tests go.
     """
+
 
 
