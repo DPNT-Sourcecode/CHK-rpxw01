611e8cd4ea811c471db03009fadebf7e6e14976d
sbx_user1051 <sbx_user1051@169.254.27.157>
2023-07-23 08:50:37 +0000

    Sun Jul 23 08:50:37 UTC 2023

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -3,6 +3,10 @@
 # noinspection PyUnusedLocal
 # friend_name = unicode string
 def hello(friend_name: str) -> str:
-    return "Hello, World!"
+    """
+    Hello world, takes an argument ``friend_name`` and returns like ``f"Hello, {friend_name}!"``.
+    """
+    return f"Hello, {friend_name}!"
+
 
 
