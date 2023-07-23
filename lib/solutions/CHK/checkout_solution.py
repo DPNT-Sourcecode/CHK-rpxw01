1433a1ec98a3bea47099687ec91dfdab66485012
sbx_user1051 <sbx_user1051@169.254.27.157>
2023-07-23 08:57:19 +0000

    Sun Jul 23 08:57:19 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -2,5 +2,13 @@
 
 # noinspection PyUnusedLocal
 # skus = unicode string
-def checkout(skus):
-    raise NotImplementedError()
+def checkout(skus: str):
+    """
+    Calculate the total price of a list of items.
+
+    The argument is a string containing the SKUs of all products in the basket.
+    The input is not well defined for now, so we will assume (lol) that it is like
+    ``AABBCCC`` to mean, eg, [2xA, 2xB, 3xC] for now,
+    and correct it later on if needed depending on how the tests go.
+    """
+
